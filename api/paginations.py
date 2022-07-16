7afd53709cd56ef2a4894eab8e4727642dc493d4
otkir <otkirgadoyev24@gmail.com>
2022-07-16 17:36:26 +0500

    pagination page_size=4

--- api/paginations.py
+++ api/paginations.py
@@ -2,7 +2,7 @@ from rest_framework import pagination
 
 
 class CustomPagination(pagination.PageNumberPagination):
-    page_size = 5
+    page_size = 4
     max_page_size =10000
     # page_size_query_param = 'page_size'
     # page_query_param = 'p'
