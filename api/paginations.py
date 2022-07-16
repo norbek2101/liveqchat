0b1b4af058065d86ec70f159deeadaefc8fe6696
otkir <otkirgadoyev24@gmail.com>
2022-07-16 14:14:52 +0500

    pagination changed

--- api/paginations.py
+++ api/paginations.py
@@ -2,10 +2,10 @@ from rest_framework import pagination
 
 
 class CustomPagination(pagination.PageNumberPagination):
-    page_size = 4
-    page_size_query_param = 'page_size'
-    max_page_size =100
-    page_query_param = 'p'
+    page_size = 5
+    max_page_size =10000
+    # page_size_query_param = 'page_size'
+    # page_query_param = 'p'
 
 
 class ChatPagination(pagination.PageNumberPagination):
