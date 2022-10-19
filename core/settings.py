78d6f25535f6be7d258e9cbf6c1b6f795ce1d8f4
Doniyor <xoja9636706@gmail.com>
2022-10-19 14:17:58 +0500

    db

--- core/settings.py
+++ core/settings.py
@@ -107,24 +107,23 @@ ASGI_APPLICATION = 'core.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-try:
-    DATABASES = {
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'liveqchat',
+#         'USER': 'liveq',
+#         'PASSWORD': 'liveq_pass',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'liveqchat',
-        'USER': 'liveq',
-        'PASSWORD': 'liveq_pass',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-except:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 
 # Password validation
